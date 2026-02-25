20c060c562bd8323b9ff3fcb3ecf3a2cb035a8da
João Alves <joao.2003.couto@gmail.com>
2026-02-25 15:05:24 +0000

    feat: install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --time=00:05:00
+#SBATCH --partition=normal-arm
+#SBATCH -A f202500010hpcvlabuminhoa
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=48
+#SBATCH --exclusive
+
+ml Python/3.13.5-GCCcore-14.3.0
+
+echo "Running on nodes:"
+scontrol show hostname $SLURM_NODELIST
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
